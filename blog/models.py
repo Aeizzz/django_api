845dbe37fccd42908ff2c6c589f68e6d5896d24d
hong-ll <1129603149@qq.com>
2017-12-18 16:52:07 +0800

    增加文章models

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from account.models import User
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Article(models.Model):
+    # 标题
+    title = models.CharField(max_length=70)
+    # 正文
+    body = models.TextField()
+    # 创建时间修改时间
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    # 摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+    #
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    #用户
+    author = models.ForeignKey(User)
